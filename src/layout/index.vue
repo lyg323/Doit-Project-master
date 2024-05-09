5b259ec99f1876f77abd8dc68a20767775f1bdd4
Lee <lyg323323@gmail.com>
2024-05-09 20:58:32 +0900

    project

---
+++ src/layout/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <section id="wrap">
+    <section>상단 영역</section>
+    <section id="container">
+      <router-view />
+    </section>
+    <section>하단 영역</section>
+  </section>
+</template>
