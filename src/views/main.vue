453fb5190ecd610f0f5959e96eb26be3faf143fd
Lee <lyg323323@gmail.com>
2024-06-13 11:47:57 +0900

    main contents - section bannermenu

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="maincontents">
     <section class="mainbanner"><img src="/images/mainimg.png" /></section>
+
     <section class="newbook">
       <h1 class="maintit">
         새로 나온 도서<span>분야별로 새로 나온 도서를 만나 보세요</span>
@@ -29,6 +30,7 @@
         </VueSlickCarousel>
       </div>
     </section>
+
     <section class="searchbook">
       <h1 class="maintit">
         도서 검색<span>찾고자 하는 도서명을 검색해 주세요</span>
@@ -62,9 +64,29 @@
         ></span>
       </div>
     </section>
-    <section class="bannermenu"></section>
+
+    <section class="bannermenu">
+      <div
+        class="itembox"
+        v-for="(item, index) in mainbanner"
+        :key="index"
+        @click="gotoPage(item.url)"
+      >
+        <span v-html="item.stitle"></span>
+        <strong v-html="item.maintit"></strong>
+        <i class="bi" :class="item.iconname"></i>
+      </div>
+    </section>
+
     <section class="notice">
-      <h1 class="maintit">공지 사항</h1>
+      <div class="noticebox">
+        <h1 class="maintit">공지 사항</h1>
+        <b-button variant="noticemore" @click="viewmore($event)" />
+        <div class="newsitem" v-for="(item, index) in noticelists" :key="index">
+          <strong v-html="item.noticetit"></strong>
+          <span v-html="item.date"></span>
+        </div>
+      </div>
     </section>
   </section>
 </template>
@@ -278,6 +300,37 @@ export default {
         },
       ],
       keyword: "",
+      mainbanner: [
+        {
+          stitle: "교수 및 강사 전용",
+          maintit: "교재 샘플/강의 자료",
+          iconname: "bi-stickies",
+          url: "/classsample",
+        },
+        {
+          stitle: "스터디 카페",
+          maintit: "Do it! 스터디룸",
+          iconname: "bi-book-half",
+          url: "https://cafe.naver.com/doitstudyroom",
+        },
+        {
+          stitle: "이지스 SNS",
+          maintit: "페이스북",
+          iconname: "bi-facebook",
+          url: "https://www.facebook.com/easyspub/",
+        },
+      ],
+      noticelists: [
+        {
+          noticetit: "이지스퍼블리싱/이지스에듀 저작물 이용 지침",
+          date: "2023.03.30",
+        },
+        { noticetit: "IT 분야 편집/기획자 모집", date: "2023.02.16" },
+        {
+          noticetit: "이지스퍼블리싱 전자책 대여 서비스 오픈!",
+          date: "2021.10.28",
+        },
+      ],
     };
   },
   created() {
@@ -288,6 +341,21 @@ export default {
       this.Newbooks = this[contents];
       document.querySelector(".active").classList.remove("active");
       event.target.classList.add("active");
+    },
+    gotoPage(url) {
+      if (url == "/classsample") {
+        this.$router.push(url);
+      } else {
+        window.open(url, "_blank");
+      }
+    },
+    viewmore(event) {
+      // indexof() 함수는 배열에서 매개변수로 전달받은 요소를 찾아 첫 번째 인덱스를 반환하고 없으면 -1을 반환한다.
+      if (event.target.parentElement.classList.value.indexOf("full") > -1) {
+        event.target.parentElement.classList.remove("full");
+      } else {
+        event.target.parentElement.classList.add("full");
+      }
     },
   },
   computed: {
